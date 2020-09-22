02c96f287ba25b5be521fe43b94c37c1d8576fb7
Guillermo Moreno <contact@memobrown.com>
2020-09-22 11:20:33 -0500

    computed properties and watchers

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ new Vue({
   data () {
     return {
       name: 'Bitcoin',
+      symbol: 'BTC',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: 0,
+      changePercent: 1,
+
+      color: 'f4f4f4',
+
+      price: 8400,
 
       pricesWithDays: [
         { day: 'Lunes', value: 8400 },
@@ -21,9 +26,23 @@ new Vue({
     }
   },
 
+  computed: {
+    title() {
+      return `${this.name} - ${this.symbol}`
+    }
+  },
+
+  watch: {
+    showPrices(newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+
   methods: {
     toggleShowPrices () {
       this.showPrices = !this.showPrices
+
+      this.color = this.color.split('').reverse().join('')
     }
   }
 })
