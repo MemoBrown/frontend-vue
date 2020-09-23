00471e33500d0e1cf79d05be7ea7304d65646236
Guillermo Moreno <contact@memobrown.com>
2020-09-23 17:52:16 -0500

    Fetch and api

--- Exchange/exchange/src/components/PxHeader.vue
+++ Exchange/exchange/src/components/PxHeader.vue
@@ -3,10 +3,14 @@
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <px-icon class="mr-2"/>
-          <span class="font-semibold text-xl tracking-tight">PlatziExchange</span>
+          <px-icon class="mr-2" />
+          <span class="font-semibold text-xl tracking-tight"
+            >PlatziExchange</span
+          >
         </div>
-        <div class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto">
+        <div
+          class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
+        >
           <div class="text-sm lg:flex-grow"></div>
         </div>
       </nav>
@@ -15,10 +19,10 @@
 </template>
 
 <script>
-import PxIcon from "@/components/PxIcon";
+import PxIcon from '@/components/PxIcon'
 
 export default {
-  name: "PxHeader",
+  name: 'PxHeader',
   components: { PxIcon }
-};
+}
 </script>
--- Exchange/exchange/src/router.js
+++ Exchange/exchange/src/router.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from '@/views/Home';
-import About from '@/views/About';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home'
+import About from '@/views/About'
 import Error from '@/views/Error'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   mode: 'history',
